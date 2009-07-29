a8e0424dd4a77c8e70a0249f8787ce556b9b3de2
Austen Ito <ito.austen@gmail.com>
2009-07-28 21:37:42 -1000

    Settings can now be accessed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.register '/register', :controller => 'users', :action => 'new'  
-  map.register '/login', :controller => 'user_sessions', :action => 'new'  
-
+  map.register '/login', :controller => 'user_sessions', :action => 'new'   
+  
   map.resource :user_session
   map.resources :bets
   map.resource :dashboard
   map.resource :bet_request
   map.resources :bet_statuses
+  map.resources :users
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
